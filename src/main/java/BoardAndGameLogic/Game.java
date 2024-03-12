b2c2db61b85eff0187961b94450126478267136d
gpasques <germain.pasques4@etu.univ-lorraine.fr>
2024-03-12 13:51:37 +0100

    Adding Night and King movements

--- src/main/java/BoardAndGameLogic/Game.java
+++ src/main/java/BoardAndGameLogic/Game.java
@@ -51,16 +51,21 @@ public class Game {
         else {
             playerMove[1] = indexes;
             playerMoveCases[1] = c;
+            Piece piece = playerMoveCases[0].getPiece();
             System.out.println(playerMove[0][0] + " " + playerMove[0][1]);
             System.out.println(playerMove[1][0] + " " + playerMove[1][1]);
             System.out.println(playerMove[0][0] - playerMove[1][0]);
             System.out.println(playerMove[0][1] - playerMove[1][1]);
             // Checking Piece type and moving accordingly
-            if(playerMoveCases[0].getPiece() instanceof Pawn) {
+            if (piece instanceof Pawn)
                 pawnMove(); // If it's a Pawn
-            } else {
+            else if (piece instanceof Night)
+                nightMove(); // If it's a Night
+            else if (piece instanceof King)
+                kingMove(); // If it's a King
+            else
                 movePiece(playerMoveCases[0], playerMoveCases[1]);
-            }
+
 
         }
     }
@@ -107,6 +112,71 @@ public class Game {
             resetPlayerMove(); // If not, reset the player move array
         else
             pawn.moved(); // If so, change the hasMoved boolean to true
+    }
+
+    /***
+     * nightMove method
+     * handle the Night movements
+     */
+    public void nightMove() {
+
+        // Get the two movement cases
+        Case c1 = playerMoveCases[0];
+        Case c2 = playerMoveCases[1];
+        // Get the pawn
+        Night night = (Night)c1.getPiece();
+        // Get the second case piece
+        Piece piece = c2.getPiece();
+        // Get movement arrays
+        int[][] nightMoves = night.getMove();
+        int[] playerMoveDiff = new int[]{
+                playerMove[0][0] - playerMove[1][0],
+                playerMove[0][1] - playerMove[1][1]
+        };
+
+        // Conditions & Logic
+        for (int[] move : nightMoves) // Going through the moves
+            if(Arrays.equals(playerMoveDiff, move)) // If the player move is valid
+                if (piece == null) // If the piece is null
+                    movePiece(c1, c2); // Move the Night
+                else if(piece.getColor() != night.getColor()) // Else if the piece is of an opposite color
+                    movePiece(c1, c2); // Move the Night
+
+        // Check is the Night moved
+        if (c1.getPiece() == night)
+            resetPlayerMove();
+    }
+
+    public void kingMove() {
+
+        // Get the two movement cases
+        Case c1 = playerMoveCases[0];
+        Case c2 = playerMoveCases[1];
+        // Get the pawn
+        King king = (King)c1.getPiece();
+        // Get the second case piece
+        Piece piece = c2.getPiece();
+        // Get movement arrays
+        int[][] kingMoves = king.getMove();
+        int[] playerMoveDiff = new int[]{
+                playerMove[0][0] - playerMove[1][0],
+                playerMove[0][1] - playerMove[1][1]
+        };
+
+        // Conditions & Logic
+        for (int[] move : kingMoves) { // Going through the moves
+            if(Arrays.equals(playerMoveDiff, move)) // If the player move is in the array
+                if(piece == null) // If the piece is null
+                    movePiece(c1, c2); // Moving the King
+                else if (piece.getColor() != king.getColor()) // If the piece is from an opposite color
+                    movePiece(c1, c2); // Moving the King
+        }
+
+        // Checking if the King moved
+        if (c1.getPiece() == king)
+            resetPlayerMove();
+        else
+            king.moved(); // Change the hasMoved boolean to true
     }
 
     public void movePiece(Case a, Case b) {
--- src/main/java/Interface/UserInterface.java
+++ src/main/java/Interface/UserInterface.java
@@ -2,17 +2,14 @@ package Interface;
 
 import BoardAndGameLogic.*;
 import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class UserInterface extends Application {
     @Override
